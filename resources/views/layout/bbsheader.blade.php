dc748cc8972b9ec374aeeaed93bf6c38e854c546
shin552 <89790908+shin552@users.noreply.github.com>
2021-10-31 12:20:03 +0900

    initial

---
+++ resources/views/layout/bbsheader.blade.php
@@ -0,0 +1,7 @@
+@section('header')
+<header class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/bbs/') }}">shinji BBS</a>
+    </div>
+</header>
+@endsection
